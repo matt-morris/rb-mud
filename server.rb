61909b41aaf0a046c937c766ee4d3fe48747ec78
Matt Morris <matt.morris@placewise.com>
2015-03-14 21:52:21 -0600

    add sketch of login/signup and a splash of color

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require "socket"
+require "colorize"
 
 class Server
   def initialize(port, host, game)
@@ -14,9 +15,16 @@ class Server
   def run
     loop do
       Thread.start(@server.accept) do |client|
-        client.puts "welcome to rb-mud..."
+        client.puts "welcome to #{'rb-mud'.red}..."
         addr = Socket.unpack_sockaddr_in(client.getpeername)
         client_id = "#{addr.last}:#{addr.first}".to_sym
+        client.puts "what is your name?"
+        name = client.gets.chomp
+        if @game.login(name)
+          client.puts "login successful".green
+        else
+          client.puts "new character..."
+        end
         @connections[client_id] = client
         listen_user_messages(client_id, client)
       end
