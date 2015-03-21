b7b725b67d0f54c9ce17adf4a82976ddfa15912c
Matt Morris <matt.morris@placewise.com>
2015-03-21 00:15:23 -0600

    add location class

---
+++ location.rb
@@ -0,0 +1,8 @@
+class Location
+  attr_accessor :name, :description, :contents, :exits
+
+  def initialize(name, description, contents = {}, exits = {})
+    @name, @description = name, description
+    @contents, @exits = contents, exits
+  end
+end
