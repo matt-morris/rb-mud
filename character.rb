71f93987351a16227c40aab0b81ecb6b519c9607
Matt Morris <matt.morris@placewise.com>
2015-03-21 16:32:36 -0600

    add description via options hash to character

--- character.rb
+++ character.rb
@@ -1,7 +1,9 @@
 class Character
-  attr_accessor :name, :level, :hp
+  attr_accessor :name, :description, :level, :hp, :location
 
-  def initialize(name, level = 1, hp = 1)
+  def initialize(name, level = 1, hp = 1, **options)
     @name, @level, @hp = name, level, hp
+    @description = options[:description] || "a generic character."
+    @location = options[:location]
   end
 end
