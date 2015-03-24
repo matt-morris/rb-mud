be8934a2a5df76a7c843a5b1a51541358895a433
Matt Morris <matt.morris@placewise.com>
2015-03-23 21:32:51 -0600

    switch to keyword arguments

--- location.rb
+++ location.rb
@@ -1,7 +1,7 @@
 class Location
   attr_accessor :name, :description, :contents, :exits
 
-  def initialize(name, description, contents = [], exits = {})
+  def initialize(name, description, contents: [], exits: {})
     @name, @description = name, description
     @contents, @exits = contents, exits
   end
